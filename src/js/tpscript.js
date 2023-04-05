b3763ef018c399cbdafd0a519bd0811d8d80efe4
Syifanp <127760295+Syifanp@users.noreply.github.com>
2023-04-05 11:19:44 +0700

    Update Button java

--- src/js/tpscript.js
+++ src/js/tpscript.js
@@ -29,5 +29,64 @@ swiperScript.addEventListener("load", function () {
     },
   });
 });
+// tombol untuk login
+const loginButton = document.getElementById('login');
+loginButton.addEventListener('click', function() {
+  window.location.href = "../thepetshop-project-master/button.html";
+});
+
+//tombol untuk rekomendasi
+const rekomendasiButton = document.getElementById('rekomendasi');
+rekomendasiButton.addEventListener('click', function() {
+  window.location.href = "../thepetshop-project-master/button.html";
+});
+
+// tombol pada slide 1
+const button1 = document.querySelector('.swiper-slide#satu button');
+button1.addEventListener('click', function() {
+  window.location.href = "../thepetshop-project-master/button.html";
+});
+
+// tombol pada slide 2
+const button2 = document.querySelector('.swiper-slide#dua button');
+button2.addEventListener('click', function() {
+  window.location.href = "../thepetshop-project-master/button.html";
+});
+
+// tombol pada slide 3
+const button3 = document.querySelector('.swiper-slide#tiga button');
+button3.addEventListener('click', function() {
+  window.location.href = "../thepetshop-project-master/button.html";
+});
+
+// tombol untuk The Shop
+const shopBtn = document.getElementById('shop');
+shopBtn.addEventListener('click', () => {
+  window.location.href = "../thepetshop-project-master/button.html";
+});
+
+// tombol untuk Pusat Perawatan
+const adopBtn = document.getElementById('adop');
+adopBtn.addEventListener('click', () => {
+  window.location.href = "../thepetshop-project-master/button.html";
+});
+
+// tombol untuk Layanan Darurat
+const SARBtn = document.getElementById('SAR');
+SARBtn.addEventListener('click', () => {
+  window.location.href = "../thepetshop-project-master/button.html";
+});
+
+// tombol untuk Berikan Donasi
+const careButton = document.getElementById('care');
+careButton.addEventListener('click', function() {
+  window.location.href = "../thepetshop-project-master/button.html";
+});
+
+// tombol untuk Baca Selengkapnya
+const shelterButton = document.getElementById('shelter');
+shelterButton.addEventListener('click', function() {
+  window.location.href = "../thepetshop-project-master/button.html";
+});
 
 document.head.appendChild(swiperScript);
